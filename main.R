91252cd6f3c4422d29e9e13d6fc1104a74344189
Carlito <karol.schewe@gmail.com>
2019-10-27 20:58:28 +0100

    first tables extracted from page

--- main.R
+++ main.R
@@ -3,12 +3,28 @@ library(jsonlite)
 library(stringr)
 
 
+#3 adres strony jest zbudowany w sposob"
+#https://wybory2001.pkw.gov.pl/
+#**0000/ <- gwiazdki to kod teryt wojewodztwa
+#SSSS**/ gdzie gwiazdka to okreg wyborczy
+#******/ kod teryt powiatu
+# /sjg2_p.html --- dla procentow na partie
+# /sjg1_p.html dla frekwencji i procenta waznych glosow
+
+
+
+# z 1997 roku wyniki są na wikipedii
 
 
 
 
 
+url<-"https://wybory2001.pkw.gov.pl/020000/SSSS01/020100/sjg1_p.html"
 
 
 
+page.contents<-read_html(url)
 
+
+
+tabelka<-html_node(page.contents,"table")
---
+++ test.R
@@ -0,0 +1,14 @@
+library(rvest)
+library(jsonlite)
+library(stringr)
+
+
+
+
+
+stronka<-"https://www.amazon.in/OnePlus-Mirror-Black-64GB-Memory/dp/B0756Z43QS?tag=googinhydr18418-21&tag=googinkenshoo-21&ascsubtag=aee9a916-6acd-4409-92ca-3bdbeb549f80"
+
+
+
+
+
